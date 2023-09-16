3259693d0d0a40af1c1709518b5f7f6a372148ed
Bailey Hamersly <baileyhamersly@gmail.com>
2023-09-16 13:01:33 -0400

    Port change

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const port = 8080;
+
+app.listen(port, () => {
+    console.log('trivia-bot listening on port', port);
+  });
+  
 const { discordClient, discordToken } = require("./src/discord/ClientConfig");
 require("./src/discord/EventHandlers");
 
-app.listen(port, () => {
-  console.log('trivia-bot listening on port', port);
-});
 
 discordClient.login(discordToken);
