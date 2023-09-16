2ee8ce9743f5ce4ff7b0296ee9530de4606c9fde
Bailey Hamersly <baileyhamersly@gmail.com>
2023-09-16 12:56:23 -0400

    Changing how app listens

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const port = 8080;
 const { discordClient, discordToken } = require("./src/discord/ClientConfig");
 require("./src/discord/EventHandlers");
+
+app.listen(port, () => {
+  console.log('trivia-bot listening on port', port);
+});
 
 discordClient.login(discordToken);
