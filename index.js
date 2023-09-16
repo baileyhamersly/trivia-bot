7b023d6690d6215c40b4e44b6cb2fc22125ebc9a
Bailey Hamersly <baileyhamersly@gmail.com>
2023-09-16 13:50:05 -0400

    Removing unnecessary code

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Hello world listening on port', port);
-});
-  
 const { discordClient, discordToken } = require("./src/discord/ClientConfig");
 require("./src/discord/EventHandlers");
 
